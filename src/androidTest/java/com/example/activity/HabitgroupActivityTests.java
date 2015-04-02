9be2b81cdedbef60224ccc9cc9b2c7470e30b1fe
tokko <andreg@kth.se>
2015-04-02 22:42:27 +0200

    save state and orientation change

---
+++ src/androidTest/java/com/example/activity/HabitgroupActivityTests.java
@@ -0,0 +1,7 @@
+package com.example.activity;
+
+/**
+ * Created by 79417 on 2015-04-02.
+ */
+public class HabitgroupActivityTests {
+}
--- src/main/java/com/tokko/habitgroups/HabitgroupActivity.java
+++ src/main/java/com/tokko/habitgroups/HabitgroupActivity.java
@@ -1,24 +1,53 @@
 package com.tokko.habitgroups;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
 
 public class HabitgroupActivity extends Activity implements HabitgroupListFragment.HabitGroupListFragmentHost{
+    private static final String TAG_EDITOR_FRAGMENT = "editor_fragment";
+    private Fragment editorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new HabitgroupListFragment()).commit();
+        if(savedInstanceState != null){
+            editorFragment = getFragmentManager().getFragment(savedInstanceState, TAG_EDITOR_FRAGMENT);
+            if(editorFragment != null)
+                getFragmentManager().beginTransaction().replace(android.R.id.content, editorFragment).addToBackStack(TAG_EDITOR_FRAGMENT).commit();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(editorFragment != null)
+            getFragmentManager().putFragment(outState, TAG_EDITOR_FRAGMENT, editorFragment);
     }
 
     @Override
     public void editHabitGroup(long id) {
-        getFragmentManager().beginTransaction().replace(android.R.id.content, HabitgroupEditor.newInstance(id)).commit();
+        showEditorFragment(id);
     }
 
     @Override
     public void addHabitGroup() {
-        getFragmentManager().beginTransaction().replace(android.R.id.content, HabitgroupEditor.newInstance()).commit();
+        showEditorFragment(-1);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(getFragmentManager().getBackStackEntryCount() > 1)
+            getFragmentManager().popBackStack();
+        else
+            finish();
+    }
+
+    private void showEditorFragment(long id){
+        editorFragment = HabitgroupEditor.newInstance(id);
+        getFragmentManager().beginTransaction().addToBackStack(TAG_EDITOR_FRAGMENT).replace(android.R.id.content, editorFragment).commit();
     }
 }
