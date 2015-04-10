5b4c167a2601dd2184849def4534bacc2c7e8674
Andreas Gustafsson <andreas.gustafsson@beamonpeople.se>
2015-04-10 16:15:41 +0200

    upgrade to junit4

--- src/test/java/com/tokko/provider/HabitProviderTests.java
+++ src/test/java/com/tokko/provider/HabitProviderTests.java
@@ -9,24 +9,19 @@ import android.net.Uri;
 import com.tokko.BuildConfig;
 
 import junit.framework.Assert;
-import junit.framework.TestCase;
 
 import org.joda.time.DateTimeConstants;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowContentResolver;
 
-import java.util.logging.Handler;
-
 @Config(emulateSdk = 19, constants = BuildConfig.class, manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
-public class HabitProviderTests extends TestCase {
+public class HabitProviderTests {
     private static final String HABIT_GROUP_PREFIX = "HabitGroup";
     private static final int NUM_HABIT_GROUPS = 20;
     private static final int NUM_HABITS = 10;
@@ -53,17 +48,17 @@ public class HabitProviderTests extends TestCase {
     @Test
     public void getHabitGroups() {
         Cursor c = mContentResolver.query(HabitProvider.URI_HABIT_GROUPS, null, null, null, HabitProvider.ID + " DESC");
-        assertNotNull(c);
-        assertEquals(NUM_HABIT_GROUPS, c.getCount());
-        assertEquals(3, c.getColumnNames().length);
-        assertTrue(c.getColumnIndex(HabitProvider.ID) >= 0);
-        assertTrue(c.getColumnIndex(HabitProvider.TIME) >= 0);
-        assertTrue(c.getColumnIndex(HabitProvider.TITLE) >= 0);
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertEquals(NUM_HABIT_GROUPS, c.getCount());
+        org.junit.Assert.assertEquals(3, c.getColumnNames().length);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.ID) >= 0);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.TIME) >= 0);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.TITLE) >= 0);
         int postfix = 0;
         long time = HABIT_GROUP_START_TIME;
         for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
-            assertEquals(HABIT_GROUP_PREFIX + postfix++, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
-            assertEquals(time, c.getLong(c.getColumnIndex(HabitProvider.TIME)));
+            org.junit.Assert.assertEquals(HABIT_GROUP_PREFIX + postfix++, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
+            org.junit.Assert.assertEquals(time, c.getLong(c.getColumnIndex(HabitProvider.TIME)));
             time += HABIT_GROUP_TIME_INCREMENT;
         }
     }
@@ -72,28 +67,28 @@ public class HabitProviderTests extends TestCase {
     public void getSingleHabitGroup() {
         String title = HABIT_GROUP_PREFIX + "3";
         Cursor c = mContentResolver.query(HabitProvider.URI_HABIT_GROUPS, null, String.format("%s=?", HabitProvider.TITLE), new String[]{title}, null);
-        assertNotNull(c);
-        assertEquals(1, c.getCount());
-        assertEquals(3, c.getColumnNames().length);
-        assertTrue(c.getColumnIndex(HabitProvider.ID) >= 0);
-        assertTrue(c.getColumnIndex(HabitProvider.TIME) >= 0);
-        assertTrue(c.getColumnIndex(HabitProvider.TITLE) >= 0);
-        assertTrue(c.moveToFirst());
-        assertEquals(title, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertEquals(1, c.getCount());
+        org.junit.Assert.assertEquals(3, c.getColumnNames().length);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.ID) >= 0);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.TIME) >= 0);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.TITLE) >= 0);
+        org.junit.Assert.assertTrue(c.moveToFirst());
+        org.junit.Assert.assertEquals(title, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
     }
 
     @Test
     public void getHabitGroupTitles() {
         Cursor c = mContentResolver.query(HabitProvider.URI_HABIT_GROUPS, new String[]{HabitProvider.TITLE}, null, null, HabitProvider.ID + " DESC");
-        assertNotNull(c);
-        assertEquals(NUM_HABIT_GROUPS, c.getCount());
-        assertEquals(1, c.getColumnNames().length);
-        assertTrue(c.getColumnIndex(HabitProvider.ID) == -1);
-        assertTrue(c.getColumnIndex(HabitProvider.TIME) == -1);
-        assertTrue(c.getColumnIndex(HabitProvider.TITLE) >= 0);
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertEquals(NUM_HABIT_GROUPS, c.getCount());
+        org.junit.Assert.assertEquals(1, c.getColumnNames().length);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.ID) == -1);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.TIME) == -1);
+        org.junit.Assert.assertTrue(c.getColumnIndex(HabitProvider.TITLE) >= 0);
         int postfix = 0;
         for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
-            assertEquals(HABIT_GROUP_PREFIX + postfix++, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
+            org.junit.Assert.assertEquals(HABIT_GROUP_PREFIX + postfix++, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
         }
     }
 
@@ -127,12 +122,12 @@ public class HabitProviderTests extends TestCase {
         String newTitle = "bananfisk2000";
         cv.put(HabitProvider.TITLE, newTitle);
         int updated = mContentResolver.update(HabitProvider.URI_HABIT_GROUPS, cv, HabitProvider.whereID(), HabitProvider.idArgs(id));
-        assertEquals(1, updated);
+        org.junit.Assert.assertEquals(1, updated);
         Cursor afterUpdate = mContentResolver.query(HabitProvider.URI_HABIT_GROUPS, null, HabitProvider.whereID(), HabitProvider.idArgs(id), null);
-        assertEquals(1, afterUpdate.getCount());
-        assertTrue(afterUpdate.moveToFirst());
-        assertEquals(time, afterUpdate.getLong(afterUpdate.getColumnIndex(HabitProvider.TIME)));
-        assertEquals(newTitle, afterUpdate.getString(afterUpdate.getColumnIndex(HabitProvider.TITLE)));
+        org.junit.Assert.assertEquals(1, afterUpdate.getCount());
+        org.junit.Assert.assertTrue(afterUpdate.moveToFirst());
+        org.junit.Assert.assertEquals(time, afterUpdate.getLong(afterUpdate.getColumnIndex(HabitProvider.TIME)));
+        org.junit.Assert.assertEquals(newTitle, afterUpdate.getString(afterUpdate.getColumnIndex(HabitProvider.TITLE)));
         afterUpdate.close();
     }
 
@@ -144,14 +139,14 @@ public class HabitProviderTests extends TestCase {
         int count = before.getCount();
         before.close();
         int deleted = mContentResolver.delete(HabitProvider.URI_HABIT_GROUPS, HabitProvider.whereID(), HabitProvider.idArgs(id));
-        assertEquals(1, deleted);
+        org.junit.Assert.assertEquals(1, deleted);
         Cursor after = mContentResolver.query(HabitProvider.URI_HABIT_GROUPS, null, null, null, null);
-        assertEquals(count-1, after.getCount());
+        org.junit.Assert.assertEquals(count - 1, after.getCount());
         for(after.moveToFirst(); !after.isAfterLast(); after.moveToNext())
-            assertTrue(id != after.getLong(after.getColumnIndex(HabitProvider.ID)));
+            org.junit.Assert.assertTrue(id != after.getLong(after.getColumnIndex(HabitProvider.ID)));
         after.close();
         after = mContentResolver.query(HabitProvider.URI_HABIT_GROUPS, null, HabitProvider.whereID(), HabitProvider.idArgs(id), null);
-        assertEquals(0, after.getCount());
+        org.junit.Assert.assertEquals(0, after.getCount());
         after.close();
     }
 
@@ -162,9 +157,9 @@ public class HabitProviderTests extends TestCase {
         long id = before.getLong(before.getColumnIndex(HabitProvider.ID));
         before.close();
         int deleted = mContentResolver.delete(HabitProvider.URI_REPEATING, HabitProvider.whereEquals(HabitProvider.HABIT_GROUP), HabitProvider.idArgs(id));
-        assertEquals(4, deleted);
+        org.junit.Assert.assertEquals(4, deleted);
         Cursor after = mContentResolver.query(HabitProvider.URI_REPEATING, null, HabitProvider.whereEquals(HabitProvider.HABIT_GROUP), HabitProvider.idArgs(id), null);
-        assertEquals(0, after.getCount());
+        org.junit.Assert.assertEquals(0, after.getCount());
         after.close();
     }
 
@@ -174,7 +169,7 @@ public class HabitProviderTests extends TestCase {
         for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext()){
             long id = c.getLong(c.getColumnIndex(HabitProvider.ID));
             Cursor c2 = mContentResolver.query(HabitProvider.URI_REPEATING, null, HabitProvider.whereEquals(HabitProvider.HABIT_GROUP), HabitProvider.idArgs(id), null);
-            assertEquals(4, c2.getCount());
+            org.junit.Assert.assertEquals(4, c2.getCount());
             c2.close();
         }
         c.close();
@@ -183,12 +178,12 @@ public class HabitProviderTests extends TestCase {
     @Test
     public void testGetHabits(){
         Cursor c = mContentResolver.query(HabitProvider.URI_HABITS, null, null, null, null);
-        assertNotNull(c);
-        assertEquals(10, c.getCount());
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertEquals(10, c.getCount());
         int i = NUM_HABITS-1;
-        for(assertTrue(c.moveToFirst()); !c.isAfterLast(); c.moveToNext()){
-            assertEquals("HABIT" + i--, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
-            assertEquals(0, c.getLong(c.getColumnIndex(HabitProvider.TIME)));
+        for(org.junit.Assert.assertTrue(c.moveToFirst()); !c.isAfterLast(); c.moveToNext()){
+            org.junit.Assert.assertEquals("HABIT" + i--, c.getString(c.getColumnIndex(HabitProvider.TITLE)));
+            org.junit.Assert.assertEquals(0, c.getLong(c.getColumnIndex(HabitProvider.TIME)));
         }
         c.close();
     }
@@ -199,11 +194,11 @@ public class HabitProviderTests extends TestCase {
         habits.moveToFirst();
         long id = habits.getLong(habits.getColumnIndex(HabitProvider.ID));
         int deleted = mContentResolver.delete(HabitProvider.URI_HABITS, HabitProvider.whereID(), HabitProvider.idArgs(id));
-        assertEquals(1, deleted);
+        org.junit.Assert.assertEquals(1, deleted);
         Cursor after = mContentResolver.query(HabitProvider.URI_HABITS, null, null, null, null);
-        assertEquals(habits.getCount()-1, after.getCount());
+        org.junit.Assert.assertEquals(habits.getCount() - 1, after.getCount());
         for(after.moveToFirst(); !after.isAfterLast(); after.moveToNext())
-            assertTrue(id != after.getLong(after.getColumnIndex(HabitProvider.ID)));
+            org.junit.Assert.assertTrue(id != after.getLong(after.getColumnIndex(HabitProvider.ID)));
         habits.close();
         after.close();
     }
@@ -218,10 +213,10 @@ public class HabitProviderTests extends TestCase {
         ContentValues cv = new ContentValues();
         cv.put(HabitProvider.TITLE, newTitle);
         int updated = mContentResolver.update(HabitProvider.URI_HABITS, cv, HabitProvider.whereID(), HabitProvider.idArgs(id));
-        assertEquals(1, updated);
+        org.junit.Assert.assertEquals(1, updated);
         habits = mContentResolver.query(HabitProvider.URI_HABITS, null, HabitProvider.whereID(), HabitProvider.idArgs(id), null);
         habits.moveToFirst();
-        assertEquals(newTitle, habits.getString(habits.getColumnIndex(HabitProvider.TITLE)));
+        org.junit.Assert.assertEquals(newTitle, habits.getString(habits.getColumnIndex(HabitProvider.TITLE)));
         habits.close();
     }
 
@@ -237,13 +232,13 @@ public class HabitProviderTests extends TestCase {
         Uri insertedUri = mContentResolver.insert(HabitProvider.URI_REPEATING, cv);
         long newRowId = ContentUris.parseId(insertedUri);
         Cursor repeating2 = mContentResolver.query(HabitProvider.URI_REPEATING, null, null, null, null);
-        assertEquals(repeating.getCount() + 1, repeating2.getCount());
+        org.junit.Assert.assertEquals(repeating.getCount() + 1, repeating2.getCount());
         Cursor c = mContentResolver.query(HabitProvider.URI_REPEATING, null, HabitProvider.whereID(), HabitProvider.idArgs(newRowId), null);
-        assertNotNull(c);
-        assertTrue(c.moveToFirst());
-        assertEquals(1, c.getCount());
-        assertEquals(weekday, c.getInt(c.getColumnIndex(HabitProvider.WEEKDAY)));
-        assertEquals(id, c.getLong(c.getColumnIndex(HabitProvider.HABIT_GROUP)));
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertTrue(c.moveToFirst());
+        org.junit.Assert.assertEquals(1, c.getCount());
+        org.junit.Assert.assertEquals(weekday, c.getInt(c.getColumnIndex(HabitProvider.WEEKDAY)));
+        org.junit.Assert.assertEquals(id, c.getLong(c.getColumnIndex(HabitProvider.HABIT_GROUP)));
 
         repeating.close();
         repeating2.close();
@@ -253,9 +248,9 @@ public class HabitProviderTests extends TestCase {
     @Test
     public void getConnections(){
         Cursor c = mContentResolver.query(HabitProvider.URI_HABITS_IN_GROUP, null, null, null, null);
-        assertNotNull(c);
-        assertTrue(c.getCount() > 0);
-        assertEquals(3, c.getColumnCount());
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertTrue(c.getCount() > 0);
+        org.junit.Assert.assertEquals(3, c.getColumnCount());
         c.close();
     }
 
@@ -265,11 +260,11 @@ public class HabitProviderTests extends TestCase {
         c.moveToFirst();
         long habitGroup = c.getLong(c.getColumnIndex(HabitProvider.HABIT_GROUP));
         int deleted = mContentResolver.delete(HabitProvider.URI_HABITS_IN_GROUP, HabitProvider.whereEquals(HabitProvider.HABIT_GROUP), HabitProvider.idArgs(habitGroup));
-        assertEquals(NUM_HABITS / 2, deleted);
+        org.junit.Assert.assertEquals(NUM_HABITS / 2, deleted);
         Cursor c2 = mContentResolver.query(HabitProvider.URI_HABITS_IN_GROUP, null, null, null, null);
-        assertEquals(c.getCount() - deleted, c2.getCount());
+        org.junit.Assert.assertEquals(c.getCount() - deleted, c2.getCount());
         for(c2.moveToFirst(); !c.isAfterLast(); c.moveToNext())
-            assertTrue(c2.getLong(c.getColumnIndex(HabitProvider.HABIT_GROUP)) != habitGroup);
+            org.junit.Assert.assertTrue(c2.getLong(c.getColumnIndex(HabitProvider.HABIT_GROUP)) != habitGroup);
         c.close();
         c2.close();
     }
@@ -289,16 +284,16 @@ public class HabitProviderTests extends TestCase {
         cv.put(HabitProvider.HABIT_GROUP, groupId);
         Uri newRowUri = mContentResolver.insert(HabitProvider.URI_HABITS_IN_GROUP, cv);
         long newId = ContentUris.parseId(newRowUri);
-        assertTrue(newId != -1);
+        org.junit.Assert.assertTrue(newId != -1);
         Cursor c2 = mContentResolver.query(HabitProvider.URI_HABITS_IN_GROUP, null, HabitProvider.whereID(), HabitProvider.idArgs(newId), null);
-        assertNotNull(c2);
-        assertTrue(c2.moveToFirst());
-        assertEquals(1, c2.getCount());
-        assertEquals(groupId, c2.getLong(c.getColumnIndex(HabitProvider.HABIT_GROUP)));
-        assertEquals(habitId, c2.getLong(c.getColumnIndex(HabitProvider.HABIT)));
+        org.junit.Assert.assertNotNull(c2);
+        org.junit.Assert.assertTrue(c2.moveToFirst());
+        org.junit.Assert.assertEquals(1, c2.getCount());
+        org.junit.Assert.assertEquals(groupId, c2.getLong(c.getColumnIndex(HabitProvider.HABIT_GROUP)));
+        org.junit.Assert.assertEquals(habitId, c2.getLong(c.getColumnIndex(HabitProvider.HABIT)));
         c2.close();
         c2 = mContentResolver.query(HabitProvider.URI_HABITS_IN_GROUP, null, null, null, null);
-        assertEquals(origCount+1, c2.getCount());
+        org.junit.Assert.assertEquals(origCount + 1, c2.getCount());
         c.close();
         c2.close();
         groups.close();
@@ -312,24 +307,24 @@ public class HabitProviderTests extends TestCase {
         long id = c.getLong(c.getColumnIndex(HabitProvider.ID));
         c.close();
         c = mContentResolver.query(HabitProvider.URI_HABITS_WITH_CONNECTION, null, HabitProvider.whereEquals(HabitProvider.HABIT_GROUP), HabitProvider.idArgs(id), null);
-        assertNotNull(c);
-        assertTrue(c.moveToFirst());
-        assertEquals(NUM_HABITS, c.getCount());
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertTrue(c.moveToFirst());
+        org.junit.Assert.assertEquals(NUM_HABITS, c.getCount());
         int connected = 0;
         for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext())
             if(c.isNull(c.getColumnIndex(HabitProvider.HABIT_GROUP))) connected++;
             else connected--;
-        assertEquals(0, connected);
+        org.junit.Assert.assertEquals(0, connected);
         c.close();
     }
 
     @Test
     public void testGetReminders(){
         Cursor c = mContentResolver.query(HabitProvider.URI_REMINDERS, null, null, null, null);
-        assertNotNull(c);
-        assertTrue(c.moveToFirst());
-        assertEquals(NUM_HABIT_GROUPS*4, c.getCount());
-        assertEquals(3, c.getColumnCount());
+        org.junit.Assert.assertNotNull(c);
+        org.junit.Assert.assertTrue(c.moveToFirst());
+        org.junit.Assert.assertEquals(NUM_HABIT_GROUPS * 4, c.getCount());
+        org.junit.Assert.assertEquals(3, c.getColumnCount());
         c.close();
     }
 }
